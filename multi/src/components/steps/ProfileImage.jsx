ad7e5cdd41c371ec0bb52c2ba3f38e706b51091e
KaNG6911 <gna.k6911@gmail.com>
2026-01-19 00:38:31 +0800

    cvjbidhsiw

--- multi/src/components/steps/ProfileImage.jsx
+++ multi/src/components/steps/ProfileImage.jsx
@@ -1,10 +1,53 @@
-import { Header } from "../layer/Header";
-import { useState, useRef } from "react";
+import { Header } from '../layer/Header'
+// import { initialValues } from '@/constants/initial'
+import { useState, useRef } from 'react'
 
 export const ProfileImage = ({ handleChange, formErrors }) => {
-  const inputRef = useRef();
-  const [isDragging, setIsDragging] = useState(false);
+  const inputRef = useRef()
+  const [isDragging, setIsDragging] = useState(false)
+  const [imageURL, setImageURL] = useState('')
+  // const [formValues, setFormValues] = useState(initialValues)
 
+  const handleBrowseClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click()
+    }
+  }
+
+  const handleUploadImage = (file) => {
+    const imageUrl = URL.createObjectURL(file)
+    setImageURL(imageUrl)
+    setFormValues((previous) => ({
+      ...previous,
+      profileImage: imageUrl,
+    }))
+  }
+
+  const clearImage = () => {
+    inputRef.current.value = ''
+    setImageURL('')
+    setFormValues((previous) => ({
+      ...previous,
+      profileImage: '',
+    }))
+  }
+  console.log(imageURL)
+
+  const handleDrop = (event) => {
+    event.preventDefault()
+    setIsDragging(false)
+    const uploadedImage = event.dataTransfer.files[0]
+    handleUploadImage(uploadedImage)
+  }
+
+  const handleDragOver = (event) => {
+    event.preventDefault()
+    setIsDragging(true)
+  }
+
+  const handleDragLeave = () => {
+    setIsDragging(false)
+  }
   return (
     <div className="flex flex-col w-150 h-200 bg-white rounded-2xl items-center">
       <Header />
@@ -22,31 +65,43 @@ export const ProfileImage = ({ handleChange, formErrors }) => {
         <div className="flex flex-col">
           Profile Image *
           <div
+            onClick={handleBrowseClick}
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
             style={{
-              display: "flex",
+              display: 'flex',
               height: 200,
               width: 480,
-              backgroundColor: "rgb(181, 180, 180)",
-              justifyContent: "center",
-              alignItems: "center",
+              backgroundColor: 'rgb(181, 180, 180)',
+              justifyContent: 'center',
+              alignItems: 'center',
               marginTop: 8,
               borderRadius: 10,
-              color: "white",
-              border: isDragging ? "2px dashed green" : "2px solid transparent",
+              color: 'white',
+              border: isDragging ? '3px dashed green' : '2px solid transparent',
             }}
           >
-            Drag and Drop
+            {imageURL ? (
+              <img
+                src={imageURL}
+                alt="preview"
+                className="flex w-50 h-50 z-10 object-cover rounded-[10px]"
+              />
+            ) : (
+              'Browse or Drag and Drop'
+            )}
             <input
+              ref={inputRef}
               type="file"
               hidden
-              name="profileImage"
               onChange={handleChange}
-              ref={inputRef}
             />
           </div>
-          <p className="err z-1">{formErrors.ProfileImage}</p>
+          <button onClick={clearImage}>X</button>
+          <p className="err">{formErrors.profileImage}</p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
--- multi/src/utils/validators.js
+++ multi/src/utils/validators.js
@@ -1,52 +1,83 @@
-import { isEmpty } from "./validation";
+import { isEmpty } from './validation'
+import { isEmail } from './validation'
+import { isPhoneNumber } from './validation'
+
+// PAGE NUMBER 1
 
 export const validateStep = (formValues) => {
-  const errors = {};
+  const errors = {}
 
   if (isEmpty(formValues.firstName)) {
-    errors.firstName = "Нэрээ оруулна уу";
+    errors.firstName = 'Нэрээ оруулна уу'
   }
   if (isEmpty(formValues.lastName)) {
-    errors.lastName = "Овгоо оруулна уу";
+    errors.lastName = 'Овгоо оруулна уу'
   }
   if (isEmpty(formValues.userName)) {
-    errors.userName = "Хэрэглэгчийн нэрээ оруулна уу";
+    errors.userName = 'Хэрэглэгчийн нэрээ оруулна уу'
   }
 
-  const isValid = Object.keys(errors).length === 0;
+  const isValid = Object.keys(errors).length === 0
 
-  return { errors, isValid };
-};
+  return { errors, isValid }
+}
+
+// PAGE NUMBER 2
 
 export const validateStepTwo = (formValues) => {
-  const errors = {};
+  const errors = {}
 
   if (isEmpty(formValues.email)) {
-    errors.email = "И-мэйл хаягаа оруулна уу";
+    errors.email = 'И-мэйл хаягаа оруулна уу'
+  } else if (!isEmail(formValues.email)) {
+    errors.email = 'И-мэйл хаяг буруу байна'
   }
+
   if (isEmpty(formValues.phoneNumber)) {
-    errors.phoneNumber = "Утасны дугаараа оруулна уу";
+    errors.phoneNumber = 'Утасны дугаараа оруулна уу'
+  } else if (!isPhoneNumber(formValues.phoneNumber)) {
+    errors.phoneNumber = 'Утасны дугаар буруу байна'
   }
+
   if (isEmpty(formValues.password)) {
-    errors.password = "Нууц үгээ оруулна уу";
+    errors.password = 'Нууц үгээ оруулна уу'
+  } else if (formValues.password.length < 6) {
+    errors.password = 'Нууц үг дор хаяж 6 тэмдэгттэй байх ёстой'
   }
   if (isEmpty(formValues.confirmPassword)) {
-    errors.confirmPassword = "Нууц үгээ давтана уу";
+    errors.confirmPassword = 'Нууц үгээ давтана уу'
   }
-  const isValid = Object.keys(errors).length === 0;
+  if (
+    !isEmpty(formValues.password) &&
+    !isEmpty(formValues.confirmPassword) &&
+    formValues.password !== formValues.confirmPassword
+  ) {
+    errors.confirmPassword = 'Нууц үг таарахгүй байна'
+  }
+  const isValid = Object.keys(errors).length === 0
 
-  return { errors, isValid };
-};
+  return { errors, isValid }
+}
+
+// PAGE NUMBER 3
+
 export const validateStepThree = (formValues) => {
-  const errors = {};
+  const errors = {}
 
   if (isEmpty(formValues.birthday)) {
-    errors.birthday = "Төрсөн огноогоо оруулна уу";
+    errors.birthday = 'Төрсөн огноогоо оруулна уу'
+  } else if (new Date(formValues.birthday) > new Date()) {
+    errors.birthday = 'Төрсөн огноо ирээдүйн огноо байж болохгүй'
+  } else if (
+    new Date().getFullYear() - new Date(formValues.birthday).getFullYear() <
+    18
+  ) {
+    errors.birthday = 'Та 18-аас дээш насны байх ёстой'
   }
   if (isEmpty(formValues.profileImage)) {
-    errors.profileImage = "Профайл зураг оруулна уу";
+    errors.profileImage = 'Профайл зураг оруулна уу'
   }
-  const isValid = Object.keys(errors).length === 0;
+  const isValid = Object.keys(errors).length === 0
 
-  return { errors, isValid };
-};
+  return { errors, isValid }
+}
